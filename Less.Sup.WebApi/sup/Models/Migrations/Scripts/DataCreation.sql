6a6ae90fc3c43eb3234c8ff56b4226c53249db98
less <info@open-source.tips>
2015-10-11 15:43:10 +0200

    data creation

--- Less.Sup.WebApi/sup/Models/Migrations/Scripts/DataCreation.sql
+++ Less.Sup.WebApi/sup/Models/Migrations/Scripts/DataCreation.sql
@@ -1,4 +1,5 @@
-﻿SELECT 'INSERT [dbo].[__MigrationHistory]([MigrationId], [ContextKey], [Model], [ProductVersion])
+﻿
+SELECT 'INSERT [dbo].[__MigrationHistory]([MigrationId], [ContextKey], [Model], [ProductVersion])
 VALUES (N''',MigrationId+''',',ContextKey+',',Model,',''',ProductVersion+''')'
 FROM dbo.__MigrationHistory
 
@@ -6,4 +7,3 @@ FROM dbo.__MigrationHistory
 SELECT 'INSERT [dbo].[WayPoints]([Id], [DbGeography], [Longitude], [Latitude], Info, RouteId)
 VALUES (',Id,',',DbGeography,',',Longitude,',',Latitude,',','''',info,''',',RouteId,')'
 FROM dbo.WayPoints 
-
